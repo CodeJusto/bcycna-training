647d723aa5a18a024d66ed34c1f416fbe0630140
Justin Wong <justincwong89@gmail.com>
2016-07-06 13:34:13 -0700

    Navbars will automatically change based on who is logged in

--- app/controllers/admin/students_controller.rb
+++ app/controllers/admin/students_controller.rb
@@ -1,7 +1,24 @@
 class Admin::StudentsController < AdminController
 
   def index
-
+    @students = Student.all
   end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update_attributes(student_params)
+    render :show
+  end
+
+protected
+
+  def student_params
+    params.require(:student).permit(:name, :email, :password, :organization)
+  end
+
 
 end
